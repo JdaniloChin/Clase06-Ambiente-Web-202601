62291873406dbe58bd974e9c558e4ae85de3cdcf
JdaniloChin <danilochin91@gmail.com>
2026-02-14 11:20:33 -0600

    complete

--- JS/usuarios.js
+++ JS/usuarios.js
@@ -18,6 +18,24 @@ document.addEventListener('DOMContentLoaded', function(){
             return;
         }
 
-        
+        const nuevaFila = tabla.insertRow();
+        nuevaFila.innerHTML = `
+        <td>${nombre}</td>
+        <td>${cedula}</td>
+        <td>${email}</td>
+        <td>${genero}</td>
+        <td>${provincia}</td>
+        <td>${direccion}</td>
+        <td>
+            <a href="#" class="btn btn-warning btn-sm btnEditar">Editar</a>
+            <a href="#" class="btn btn-danger btn-sm"
+            onclick="return confirm('¿Está seguro de eliminar este usuario?')">Eliminar</a>
+        </td>
+        `;
+
+        form.reset();
+
+        const modal = bootstrap.Modal.getInstance(document.getElementById('modalFormulario'));
+        modal.hide();
     });
 });
